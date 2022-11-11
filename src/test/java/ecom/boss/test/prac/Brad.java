458013fa3151bd4f36ffd82975506d86a7d42973
GowdhamHatake <gowdhamandrewssingh@gmail.com>
2022-11-11 10:17:16 +0530

    first push to master

---
+++ src/test/java/ecom/boss/test/prac/Brad.java
@@ -0,0 +1,27 @@
+package ecom.boss.test.prac;
+
+public class Brad {
+	
+private void fir() {
+
+	System.out.println(2);
+}
+
+private void sec() {
+
+	System.out.println(3);
+}
+
+private void thi() {
+
+	System.out.println(4);
+}
+
+
+
+
+
+
+
+
+}
